8fe231a3be61c256f2c4698433d4aacd71ac3861
Alexandru Patranescu <drealecs@gmail.com>
2015-05-25 22:08:12 +0300

    remove unstarted from test name

--- tests/TimeBenchmark/StopwatchTest.php
+++ tests/TimeBenchmark/StopwatchTest.php
@@ -26,7 +26,7 @@ class StopwatchTest extends \PHPUnit_Framework_TestCase
         $this->assertTrue($stopWatch->wasStopped());
     }
 
-    public function testCreateUnstartedAndStopStatus()
+    public function testCreateAndStopStatus()
     {
         $stopWatch = Stopwatch::create();
 
